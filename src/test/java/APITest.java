396825025aac6a1f310888594d80c47e55d2ba23
Сергей Серебряков <serzh.serebryakov.96@gmail.com>
2023-09-05 22:43:57 +0700

    add homework

---
+++ src/test/java/APITest.java
@@ -0,0 +1,29 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class APITest {
+        private final int unexistingPetId = 49841568;
+
+        @BeforeEach
+        public void setup() {
+            RestAssured.baseURI = "https://petstore.swagger.io/v2/";
+        }
+
+
+        @Test
+        public void petNotFoundTest_BDD() {
+            given().when()
+                    .get(baseURI + "pet/{id}", unexistingPetId)
+                    .then()
+                    .log().all()
+                    .statusCode(404)
+                    .statusLine("HTTP/1.1 404 Not Found")
+                    .body("message", equalTo("Pet not found"))
+                    .body("type", equalTo("error"));
+        }
+    }
