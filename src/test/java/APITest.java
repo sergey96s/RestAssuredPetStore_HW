453aeddb78b01864a6189abb8d9402a67170460c
Сергей Серебряков <serzh.serebryakov.96@gmail.com>
2023-09-05 22:46:25 +0700

    Отформатировал

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -7,23 +7,23 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class APITest {
-        private final int unexistingPetId = 49841568;
+    private final int unexistingPetId = 49841568;
 
-        @BeforeEach
-        public void setup() {
-            RestAssured.baseURI = "https://petstore.swagger.io/v2/";
-        }
-
-
-        @Test
-        public void petNotFoundTest_BDD() {
-            given().when()
-                    .get(baseURI + "pet/{id}", unexistingPetId)
-                    .then()
-                    .log().all()
-                    .statusCode(404)
-                    .statusLine("HTTP/1.1 404 Not Found")
-                    .body("message", equalTo("Pet not found"))
-                    .body("type", equalTo("error"));
-        }
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = "https://petstore.swagger.io/v2/";
     }
+
+
+    @Test
+    public void petNotFoundTest_BDD() {
+        given().when()
+                .get(baseURI + "pet/{id}", unexistingPetId)
+                .then()
+                .log().all()
+                .statusCode(404)
+                .statusLine("HTTP/1.1 404 Not Found")
+                .body("message", equalTo("Pet not found"))
+                .body("type", equalTo("error"));
+    }
+}
